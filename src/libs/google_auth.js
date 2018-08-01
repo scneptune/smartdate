2a005af4df0e0453be6910a7cf57854157ce6855
Stephen Neptune <steven.neptune@gmail.com>
2018-08-01 10:11:20 -0700

    firebase google auth stuff.

--- src/libs/google_auth.js
+++ src/libs/google_auth.js
@@ -1,14 +1,20 @@
 import { GoogleSignin } from 'react-native-google-signin';
-import * as firebase from 'react-native-firebase';
+import firebase from 'react-native-firebase';
 
 // Calling this function will open Google for login.
 export const googleLogin = async () => {
   try {
     // Add any configuration settings here:
-    await GoogleSignin.configure();
+    await GoogleSignin.configure({
+      forceConsentPrompt: true,
+    });
 
     const data = await GoogleSignin.signIn();
+    console.info(data);
 
+    console.info(GoogleSignin)
+
+    console.info(accessToken);
     // create a new firebase credential with the token
     const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken)
     // login with credential
